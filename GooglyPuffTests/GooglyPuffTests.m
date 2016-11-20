bee9c467ca2231d2577c8cb78b183f49c55e7ace
Yuen-rMBP <rxg9527@sina.cn>
2016-11-21 00:20:04 +0800

    dispatch_semaphore 在测试运行时，注意 Xcode debug 导航栏里的 CPU 使用率。这个设计不当的实现就是一个基本的 自旋锁 。它很不实用，因为你在 While 循环里浪费了珍贵的 CPU 周期；而且它也几乎没有扩展性。 译者注：所谓自旋锁，就是某个线程一直抢占着 CPU 不断检查以等到它需要的情况出现。因为现代操作系统都是可以并发运行多个线程的，所以它所等待的那个线程也有机会被调度执行，这样它所需要的情况早晚会出现。 需要一个更优雅、可扩展的解决方案来阻塞线程直到资源可用。 信号量让你控制多个消费者对有限数量资源的访问。举例来说，如果你创建了一个有着两个资源的信号量，那同时最多只能有两个线程可以访问临界区。其他想使用资源的线程必须在一个FIFO队列里等待

--- GooglyPuffTests/GooglyPuffTests.m
+++ GooglyPuffTests/GooglyPuffTests.m
@@ -45,18 +45,26 @@ const int64_t kDefaultTimeoutLengthInNanoSeconds = 10000000000; // 10 Seconds
 
 - (void)downloadImageURLWithString:(NSString *)URLString
 {
+    // 1创建一个信号量。参数指定信号量的起始值。这个数字是你可以访问的信号量，不需要有人先去增加它的数量。（注意到增加信号量也被叫做发射信号量）。译者注：这里初始化为0，也就是说，有人想使用信号量必然会被阻塞，直到有人增加信号量。
+    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+    
     NSURL *url = [NSURL URLWithString:URLString];
-    __block BOOL isFinishedDownloading = NO;
     __unused Photo *photo = [[Photo alloc]
                              initwithURL:url
                              withCompletionBlock:^(UIImage *image, NSError *error) {
                                  if (error) {
                                      XCTFail(@"%@ failed. %@", URLString, error);
                                  }
-                                 isFinishedDownloading = YES;
+                                 
+                                 // 2在 Completion Block 里你告诉信号量你不再需要资源了。这就会增加信号量的计数并告知其他想使用此资源的线程。
+                                 dispatch_semaphore_signal(semaphore);
                              }];
     
-    while (!isFinishedDownloading) {}
+    // 3
+    dispatch_time_t timeoutTime = dispatch_time(DISPATCH_TIME_NOW, kDefaultTimeoutLengthInNanoSeconds);
+    if (dispatch_semaphore_wait(semaphore, timeoutTime)) {
+        XCTFail(@"%@ timed out", URLString);
+    }
 }
 
 @end
